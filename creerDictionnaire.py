9ff90f797beaa96b934856519cf29cbff017d0a1
Baptiste1331 <baptiste.cadoux@ecole.ensicaen.fr>
2024-09-10 12:00:21 +0200

    code dictionnaire

---
+++ creerDictionnaire.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+def creerDictionnaire(chemin_csv, chemin_images):
+
+    dictionnaire = {}
+
+    if chemin_images:
+        liste_image = os.listdir(chemin_images)
+    
+    if chemin_csv:
+        csv = pd.read_csv(chemin_csv)
+    
+    for i in range (len(csv)):
+        image = csv.iloc[i]["filename"]
+
+        if image in liste_image:
+            coordonnees = [int(csv.iloc[i]["xmin"]), int(csv.iloc[i]["ymin"]), int(csv.iloc[i]["xmax"]), int(csv.iloc[i]["ymax"])]
+        
+        if image in dictionnaire:
+            dictionnaire[image].append(coordonnees)
+        else:
+            dictionnaire[image] = [coordonnees]
+
+    return dictionnaire
+
+
+# EXEMPLE
+# creerDictionnaire(r"C:\Users\bapti\Desktop\PlantDoc-Object-Detection-Dataset-master\test_labels.csv", r"C:\Users\bapti\Desktop\PlantDoc-Object-Detection-Dataset-master\TEST")
+
+    
